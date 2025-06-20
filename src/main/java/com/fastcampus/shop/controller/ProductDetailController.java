47b9bfaf2ecf85719c3dbc0367aa4320131c2abe
skykim5538@gmail.com <skykim5538@gmail.com>
2025-06-20 14:10:48 +0900

    수정 ì 사항 반영

--- src/main/java/com/fastcampus/shop/controller/ProductDetailController.java
+++ src/main/java/com/fastcampus/shop/controller/ProductDetailController.java
@@ -1,12 +1,11 @@
 package com.fastcampus.shop.controller;
 
-import com.fastcampus.shop.ProductVO.ProductVO;
+import com.fastcampus.shop.ProductDto.ProductDto;
 import com.fastcampus.shop.mapper.ProductMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,7 +22,7 @@ public class ProductDetailController {
     public String detail(@PathVariable("productId") String productId, HttpServletRequest request, Model model) {
 
 
-          ProductVO detailPage = productMapper.detailPage(productId);
+          ProductDto detailPage = productMapper.detailPage(productId);
 
            model.addAttribute("productDetail", detailPage);
 
