35fbabbbc2ac5b4098aa8dba81291343be1ff288
skykim5538@gmail.com <skykim5538@gmail.com>
2025-06-20 14:10:48 +0900

    수정 ì 사항 반영

--- src/main/java/com/fastcampus/shop/mapper/ProductMapper.java
+++ src/main/java/com/fastcampus/shop/mapper/ProductMapper.java
@@ -1,28 +1,27 @@
 package com.fastcampus.shop.mapper;
 
 
-import com.fastcampus.shop.ProductVO.ProductVO;
+import com.fastcampus.shop.ProductDto.ProductDto;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
 
-    ProductVO detailPage(String productId);
+    ProductDto detailPage(String productId);
 
     // 전체 조회 (필요 시 페이지네이션 추가)
-    List<ProductVO> list();
+    List<ProductDto> list();
 
     // Insert (필요 시 사용)
-    int insert(ProductVO productDetail);
+    int ProductInsert(ProductDto productDetail);
 
     // Update (필요 시)
-    int update(ProductVO productDetail);
+    int ProductUpdate(ProductDto productDetail);
 
     // Delete (필요 시)
-    int delete(int productDetailId);
+    int ProductDelete(int productDetailId);
 }
 
 
