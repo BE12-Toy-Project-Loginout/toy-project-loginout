4fe3b2150c57af9b34d17ad9d8da228a1f9e0d57
daamont <daamont01@gmail.com>
2025-06-20 16:12:33 +0900

    chore: RequestMapping 애너테이션 추가  Changes to be committed: 	modified:   src/main/java/com/fastcampus/shop/controller/ProductDetailController.java

--- src/main/java/com/fastcampus/shop/controller/ProductDetailController.java
+++ src/main/java/com/fastcampus/shop/controller/ProductDetailController.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/product")
 public class ProductDetailController {
 
 
